ff1ae55d0668db18d8cb625dd0990303bccb98e1
dnantonio <46569135+dnantonio@users.noreply.github.com>
2019-11-13 15:04:41 +0000

    Added potential temperature function

--- example_project/example_project.py
+++ example_project/example_project.py
@@ -10,3 +10,15 @@ def fahrToKelv(temp):
     kelvin = 5./9. * (temp - 32.) + 273.15
 
     return kelvin
+
+def potTemp(t, p):
+    
+    # Computes potential temperature for given temperature t (in K) and pressure p (in hPa)
+    # assuming standard air surface pressure
+    
+    p0 = 1013.25
+    r_cp = 0.286
+    
+    theta = T * (p0/p)**r_cp
+    
+    return(theta)
