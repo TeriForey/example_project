847273833a53b0f2336bfac4a5ea9068c67a2e8e
dan-orr <55704971+dan-orr@users.noreply.github.com>
2019-11-13 15:02:49 +0000

    Update example_project.py

--- example_project/example_project.py
+++ example_project/example_project.py
@@ -7,6 +7,6 @@ def fahrToKelv(temp):
     takes a temperature `temp` in fahrenheit and returns it in Kelvin
     """
 
-    kelvin = 5./9. * (temp - 32.) + 273.15
+    kelvin = 5./9. * (temp - 32.) + 373.15
 
     return kelvin
