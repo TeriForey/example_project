87bc6b2b1ed6a0efb81d21edbf49afccda2bc2b4
Nikoleta Kalaitzi <33627763+nk274@users.noreply.github.com>
2019-11-13 15:02:31 +0000

    Update example_project.py

--- example_project/example_project.py
+++ example_project/example_project.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-
+import scipy as sp
 
 def fahrToKelv(temp):
     """
     takes a temperature `temp` in fahrenheit and returns it in Kelvin
     """
-
+    a=a+1
     kelvin = 5./9. * (temp - 32.) + 273.15
 
     return kelvin
