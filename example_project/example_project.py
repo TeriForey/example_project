ca31833aea8388f54255783e9a77eafe751edf3b
rjp23 <31730876+rjp23@users.noreply.github.com>
2019-11-13 15:03:00 +0000

    Update example_project.py
    added function to reverse calc

--- example_project/example_project.py
+++ example_project/example_project.py
@@ -10,3 +10,13 @@ def fahrToKelv(temp):
     kelvin = 5./9. * (temp - 32.) + 273.15
 
     return kelvin
+
+def KelvTofahr(temp):
+    """
+    takes a temperature `temp` in Kelvin and returns fahrenheit 
+    """
+    
+    fahr = (temp + 32.) * 9/5. - 273.15
+
+
+    return fahr
